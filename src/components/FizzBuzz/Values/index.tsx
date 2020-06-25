ec3720b5aad5334e5371cd9bc8fea2196d06a4df
tangoyankee <miller.tim108@gmail.com>
2020-06-25 16:54:41 -0700

    basic outline of values component

--- src/components/FizzBuzz/Values/index.tsx
+++ src/components/FizzBuzz/Values/index.tsx
@@ -3,15 +3,27 @@ import * as React from 'react'
 import { FizzBuzz } from 'components/FizzBuzz'
 
 type ValuesProps = {
-    toggleValuesAreShown: FizzBuzz['toggleValuesAreShown']
+  toggleValuesAreShown: FizzBuzz['toggleValuesAreShown'],
+  fizzValue: number,
+  buzzValue: number,
+  updateFizz: FizzBuzz['updateFizz'],
+  updateBuzz: FizzBuzz['updateBuzz']
 }
 
-export const Values = ({ toggleValuesAreShown }: ValuesProps) => {
+export const Values = ({ toggleValuesAreShown, fizzValue, buzzValue, updateFizz, updateBuzz }: ValuesProps) => {
   return (
     <div>
-      <h3>Values Component</h3>
+      <p>Please enter a fizz and buzz time in seconds. <strong>Values should be 2 to 10, inclusive</strong></p>
+      <label>
+        Fizz:
+        <input type="text" id="fizzValue" name="fizzValue" value={fizzValue} onChange={updateFizz} disabled={false}/>
+      </label>
+      <label>
+        Buzz:
+        <input type="text" id="buzzValue" name="buzzValue" value={buzzValue} onChange={updateBuzz} disabled={false}/>
+      </label>
       <button onClick={toggleValuesAreShown}>
-                Go to Timer
+        Go to Timer &gt;
       </button>
     </div>
   )
--- src/components/FizzBuzz/index.tsx
+++ src/components/FizzBuzz/index.tsx
@@ -3,17 +3,23 @@ import * as React from 'react'
 import { Timer } from 'components/FizzBuzz/Timer'
 import { Values } from 'components/FizzBuzz/Values'
 
-type MyState = {
-    valuesAreShown: boolean
+type FizzBuzzState = {
+  valuesAreShown: boolean
+  fizzValue: number
+  buzzValue: number
 }
 
-export class FizzBuzz extends React.Component<{}, MyState> {
+export class FizzBuzz extends React.Component<{}, FizzBuzzState> {
   constructor (props: any) {
     super(props)
     this.state = {
-      valuesAreShown: true
+      valuesAreShown: true,
+      fizzValue: 2,
+      buzzValue: 10
     }
     this.toggleValuesAreShown = this.toggleValuesAreShown.bind(this)
+    this.updateFizz = this.updateFizz.bind(this)
+    this.updateBuzz = this.updateBuzz.bind(this)
   }
 
   toggleValuesAreShown () {
@@ -22,9 +28,41 @@ export class FizzBuzz extends React.Component<{}, MyState> {
     })
   }
 
+  updateFizz: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const fizzIn: number = Number(event.target.value)
+    if (Number.isInteger(fizzIn)) {
+      this.setState({
+        fizzValue: fizzIn
+      })
+    } else {
+      this.setState({
+        fizzValue: 2
+      })
+    }
+  }
+
+  updateBuzz: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const buzzIn: number = Number(event.target.value)
+    if (Number.isInteger(buzzIn)) {
+      this.setState({
+        buzzValue: buzzIn
+      })
+    } else {
+      this.setState({
+        buzzValue: 3
+      })
+    }
+  }
+
   renderValues () {
     return (
-      <Values toggleValuesAreShown={() => this.toggleValuesAreShown()} />
+      <Values
+        toggleValuesAreShown={() => this.toggleValuesAreShown()}
+        fizzValue={this.state.fizzValue}
+        buzzValue={this.state.buzzValue}
+        updateFizz={(event) => this.updateFizz(event)}
+        updateBuzz={(event) => this.updateBuzz(event)}
+      />
     )
   }
 
